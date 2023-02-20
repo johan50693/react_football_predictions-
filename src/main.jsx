ff4d850e69d72d69af25a20b8090d321a5cd7e81
Albert Urbina <johan50693@gmail.com>
2023-02-19 20:09:27 -0400

    Ajuste en nombre de archivo para la compilacion

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
-import { FootballApp } from './footballApp'
+import { FootballApp } from './FootballApp'
 import store from './store/store'
 
 
