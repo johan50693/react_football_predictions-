a04337ab528f88aa22b6150872dbc8a358cdc9ff
Albert Urbina <johan50693@gmail.com>
2023-02-19 20:38:23 -0400

    Ajuste de llamado a variables de entorno para compolacion en prod

--- src/apis/footballApi.js
+++ src/apis/footballApi.js
@@ -1,11 +1,11 @@
 import axios from "axios"
-import { getEnvVariables } from "../helpers"
+// import { getEnvVariables } from "../helpers"
 
 
-const { VITE_API_URL_BACKEND } = getEnvVariables(); 
+// const { VITE_API_URL_BACKEND } = getEnvVariables(); 
 
 const footballApi = axios.create({
-  baseURL: VITE_API_URL_BACKEND
+  baseURL: import.meta.env.VITE_API_URL_BACKEND
 })
 
 // Definición de interceptores
